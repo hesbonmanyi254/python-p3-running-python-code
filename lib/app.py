88d38e61b84adf604150fc8366ef8aa90c7fa583
hesbonmanyi254 <hesbonmanyinsa96@gmail.com>
2023-12-04 14:32:37 +0300

    done

---
+++ lib/app.py
@@ -0,0 +1,13 @@
+# lib/app.py
+print("Hello world!")
+
+
+# lib/app.py
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
